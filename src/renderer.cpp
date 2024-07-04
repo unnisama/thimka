da22a9aed7d541d769367fe2cd7d56e3558e2a5f
unnisama <gola.lola17@gmail.com>
2024-07-04 22:39:34 +0530

    iu

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -3,7 +3,6 @@
 void Renderer::Draw(VertexArray &va, IndexBuffer &ib, Shader &shader)
 {
     va.Bind();
-    ib.Bind();
     GLDEBUGCALL(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
 }
 
